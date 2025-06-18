80104471e798b58a3578d505efe9dd46536530f1
Mostasir Mahim <mahimlive@gmail.com>
2025-06-18 15:13:40 +0600

    Java Files 2.0

---
+++ Introduction.java
@@ -0,0 +1,185 @@
+Variables in Java : 
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		int x = 123;
+		double y = 3.14;
+		boolean z = true;
+		char symbol = '@';
+		String name = "Bro";
+		
+		System.out.println("Hello "+name);
+		
+	}
+}
+
+How to swap 2 variables 
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String x = "water";
+		String y = "Kool-Aid";
+		String temp;
+		
+		temp = x;
+		x=y;
+		y=temp;
+		
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+	}
+}
+accept user input in Java
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is your name? ");
+		String name = scanner.nextLine();
+		
+		System.out.println("How old are you? ");
+		int age = scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("What is your favorite food?");
+		String food = scanner.nextLine();
+			
+		System.out.println("Hello "+name);
+		System.out.println("You are "+age+" years old");
+		System.out.println("You like "+food);
+
+                scanner.close();
+	}	
+}
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is your name? ");
+		String name = scanner.nextLine();
+		
+		System.out.println("How old are you? ");
+		int age = scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("What is your favorite food?");
+		String food = scanner.nextLine();
+			
+		System.out.println("Hello "+name);
+		System.out.println("You are "+age+" years old");
+		System.out.println("You like "+food);
+
+                scanner.close();
+	}	
+}
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is your name? ");
+		String name = scanner.nextLine();
+		
+		System.out.println("How old are you? ");
+		int age = scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("What is your favorite food?");
+		String food = scanner.nextLine();
+			
+		System.out.println("Hello "+name);
+		System.out.println("You are "+age+" years old");
+		System.out.println("You like "+food);
+
+                scanner.close();
+	}	
+}
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is your name? ");
+		String name = scanner.nextLine();
+		
+		System.out.println("How old are you? ");
+		int age = scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("What is your favorite food?");
+		String food = scanner.nextLine();
+			
+		System.out.println("Hello "+name);
+		System.out.println("You are "+age+" years old");
+		System.out.println("You like "+food);
+
+                scanner.close();
+	}	
+}
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is your name? ");
+		String name = scanner.nextLine();
+		
+		System.out.println("How old are you? ");
+		int age = scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("What is your favorite food?");
+		String food = scanner.nextLine();
+			
+		System.out.println("Hello "+name);
+		System.out.println("You are "+age+" years old");
+		System.out.println("You like "+food);
+
+                scanner.close();
+	}	
+}
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What is your name? ");
+		String name = scanner.nextLine();
+		
+		System.out.println("How old are you? ");
+		int age = scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("What is your favorite food?");
+		String food = scanner.nextLine();
+			
+		System.out.println("Hello "+name);
+		System.out.println("You are "+age+" years old");
+		System.out.println("You like "+food);
+
+                scanner.close();
+	}	
+}
